e06dc63690e110a98ca2ac1f9d17b18227fbb3dc
Kabir Hossain <kabir.hossain@nitex.info>
2025-08-04 23:09:21 +0600

    feat: initial commit

---
+++ src/lib/hooks.ts
@@ -0,0 +1,12 @@
+'use client';
+
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from 'react-redux';
+import type { RootState, AppDispatch } from './store';
+
+// Custom hooks for Redux
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
